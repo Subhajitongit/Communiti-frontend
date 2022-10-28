d48e40acc15c51391c33aa702f5e86d55236d643
Subhajit Gorai <subhajitthisside@gmail.com>
2022-10-28 22:14:59 +0530

    Communiti frontend

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+export default function Welcome() {
+  const [userName, setUserName] = useState("");
+  useEffect(async () => {
+    setUserName(
+      await JSON.parse(
+        localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+      ).username
+    );
+  }, []);
+  return (
+    <Container>
+      <iframe src="https://giphy.com/embed/dzaUX7CAG0Ihi"></iframe>
+      <h1>
+        Welcome, <span>{userName}!</span>
+      </h1>
+      <h3>Please select a chat to Start messaging.</h3>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  iframe {
+    border: none;
+    width: fit-content;
+    height: fit-content;
+    border-radius: 10px;
+    margin-bottom: 1rem;
+  }
+  span {
+    color: #ff0e4a;
+  }
+`;
